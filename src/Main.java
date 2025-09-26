cbfad5bcea387e821ebf90907bb60217d171c81a
Anton <93573856+gluck243@users.noreply.github.com>
2025-09-26 16:53:02 +0300

    Delete src/Main.java
    Deleting deprecated files

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // MyFrame myFrame = new MyFrame();
-        // TextFieldFrame tf = new TextFieldFrame();
-        // new MenuBarFrame();
-        // OptionFrame optionFrame = new OptionFrame();
-        new CalculatorFrame();
-    }
-}
